9936bb048e012008e35c6283849034514bb7fe67
Dmitriy <dmitriypruss21041984@gmail.com>
2021-06-09 22:46:41 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -3,32 +3,37 @@
 // Использовать бинарный поиск, чтобы отгадать загаданное пользователем число от 0 до 100.
 // Задавать вопрос с помощью confirm.
 
-
 function searchValue() {
 
     const num = +prompt('Enter a number');
-    
+
+    if (typeof num !== 'number') {
+        throw new RangeError('num is not a number!');
+    };
+    if (Number.isNaN(num) || !Number.isSafeInteger(num) || num < 0 || num > 100 ) {
+        throw new RangeError('False value for num!');
+    };
 
     let start = 0;
     let end = 100;
     let middle;
 
-    for(let i = start; i <= end; i++) {
+    for(let i = 0; ; i++) {
 
-        console.log('start :>> ', start);
-        console.log('end :>> ', end);
+        middle = Math.ceil( (start + end) / 2 );
+        const question = confirm(`Число больше либо равно ${middle}? Спойлер - ${num} )`);
 
-        middle = Math.floor( (start + end) / 2 );
-        const question = confirm(`Число больше либо равно ${middle}? Спойлер) - ${num}`);
+        if (num < start || num > end) {
+            throw new RangeError('False range sequence!');
+        };
 
-        
         if (num === middle) {
             return num;
         } else if (question) {
-            start = middle;
+            start = middle + 1;
         } else {
-            end = middle;
-        }
+            end = middle - 1;
+        };
     };
 };
 
